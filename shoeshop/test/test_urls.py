9df4ed1838ac9a4bfe8c11228d8e013dc463b3fc
bekatadesse <bekatadesse5@gmail.com>
2023-01-03 18:58:46 +0300

    started testing Urls

---
+++ shoeshop/test/test_urls.py
@@ -0,0 +1,32 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from shoeshop.views import *
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+
+class TestUrls(SimpleTestCase):
+    def test_home_view_is_resolved(self):
+        url = reverse('')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, home_view)
+    
+    def test_afterlogin_view_is_resolved(self):
+        url = reverse('afterlogin')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, afterlogin_view)
+    
+    # def test_LogoutView_view_is_resolved(self):
+    #     url = reverse('logout')
+    #     print(resolve(url))
+    #     self.assertEqual(resolve(url).func, LogoutView)
+    
+    def test_contactus_view_is_resolved(self):
+        url = reverse('contactus')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, contactus_view)
+
+    def test_search_view_is_resolved(self):
+        url = reverse('search')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, search_view)        
